3474b1b0109c2142960da6d3f668528c7bf05304
DanielTruong999 <datthanh01237344449@gmail.com>
2024-10-08 11:05:03 +0900

    first commit

--- exts/rl_robot/setup.py
+++ exts/rl_robot/setup.py
@@ -1,4 +1,4 @@
-"""Installation script for the 'ext_template' python package."""
+"""Installation script for the 'rl_robot' python package."""
 
 import os
 import toml
@@ -18,8 +18,8 @@ INSTALL_REQUIRES = [
 
 # Installation operation
 setup(
-    name="ext_template",
-    packages=["ext_template"],
+    name="rl_robot",
+    packages=["rl_robot"],
     author=EXTENSION_TOML_DATA["package"]["author"],
     maintainer=EXTENSION_TOML_DATA["package"]["maintainer"],
     url=EXTENSION_TOML_DATA["package"]["repository"],
